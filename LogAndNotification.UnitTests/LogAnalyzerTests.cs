900a2e6ec60d64f003a294a7719c69046a2e994d
Ghidyon <gideonakunana@gmail.com>
2022-03-21 12:25:47 +0100

    added setup

--- LogAndNotification.UnitTests/LogAnalyzerTests.cs
+++ LogAndNotification.UnitTests/LogAnalyzerTests.cs
@@ -6,16 +6,21 @@ namespace LogAndNotification.UnitTests
     [TestFixture]
     public class LogAnalyzerTests
     {
+        private LogAnalyzer _analyzer = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            _analyzer = new LogAnalyzer();
+        }
+
         [TestCase("filewithgoodextension.SLF", true)]
         [TestCase("filewithgoodextension.slf", true)]
         [TestCase("unexpectedly.foo", false)]
         public void IsValidLogFileName_ValidExtensions_ChecksThem(string fileName, bool expected)
         {
-            // Arrange
-            LogAnalyzer logAnalyzer = new LogAnalyzer();
-
             //Act
-            bool result = logAnalyzer.IsValidLogFileName(fileName);
+            bool result = _analyzer.IsValidLogFileName(fileName);
 
             //Assert
             Assert.AreEqual(expected, result);
